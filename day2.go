fa7ae72f42bf5e491919b3d54e6ca0976e31c5db
Pertti Viitala <pertti.viitala@gmail.com>
2021-12-02 18:08:44 +0200

    Update comments on second day

--- day2.go
+++ day2.go
@@ -10,25 +10,21 @@ import (
 
 func main() {
 	// It seems like the submarine can take a series of commands like forward 1,
-	// down 2, or up 3:
-	//
-	// - forward X increases the horizontal position by X units.
-	// - down X increases the depth by X units.
-	// - up X decreases the depth by X units.
+	// down 2, or up 3
 	lines := readInputLines()
 
-	// Calculate the horizontal position and depth you would have after following
-	// the planned course.
-	fmt.Println(problemA(lines))
+	// What do you get if you multiply your final horizontal position by your
+	// final depth?
+	if horisontal, depth := problemA(lines); true {
+		fmt.Println(horisontal * depth)
+	}
 
-	// The commands also mean something entirely different than you first thought:
-	//
-	// - down X increases your aim by X units.
-	// - up X decreases your aim by X units.
-	// - forward X does two things:
-	//   - It increases your horizontal position by X units.
-	//   - It increases your depth by your aim multiplied by X.
-	fmt.Println(problemB(lines))
+	// Based on your calculations, the planned course doesn't seem to make any
+	// sense. You find the submarine manual and discover that the process is
+	// actually slightly more complicated.
+	if horisontal, depth := problemB(lines); true {
+		fmt.Println(horisontal * depth)
+	}
 }
 
 func readInputLines() (lines []string) {
@@ -45,11 +41,13 @@ func readInputLines() (lines []string) {
 	return
 }
 
-// What do you get if you multiply your final horizontal position by your
-// final depth?
-func problemA(input []string) int {
-	horisontal := 0
-	depth := 0
+// Calculate the horizontal position and depth you would have after following
+// the planned course.
+//
+// - forward X increases the horizontal position by X units.
+// - down X increases the depth by X units.
+// - up X decreases the depth by X units.
+func problemA(input []string) (horisontal, depth int) {
 	for _, command := range input {
 		parts := strings.Fields(command)
 		amount, err := strconv.Atoi(parts[1])
@@ -66,14 +64,18 @@ func problemA(input []string) int {
 			depth += amount
 		}
 	}
-	return horisontal * depth
+	return
 }
 
-// What do you get if you multiply your final horizontal position by your
-// final depth?
-func problemB(input []string) int {
-	horisontal := 0
-	depth := 0
+// Calculate the horizontal position and depth you would have after following
+// the planned course.
+//
+// - down X increases your aim by X units.
+// - up X decreases your aim by X units.
+// - forward X does two things:
+//   - It increases your horizontal position by X units.
+//   - It increases your depth by your aim multiplied by X.
+func problemB(input []string) (horisontal, depth int) {
 	aim := 0
 	for _, command := range input {
 		parts := strings.Fields(command)
@@ -92,5 +94,5 @@ func problemB(input []string) int {
 			aim += amount
 		}
 	}
-	return horisontal * depth
+	return
 }
